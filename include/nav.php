6f7cd2ef02c80c89293c65f37113ccbb51be21a6
Giovanni Canzio <giovanni.canzio@gmail.com>
2015-06-09 23:11:26 +0200

    adding active items mechanism in nav menu

--- include/nav.php
+++ include/nav.php
@@ -1,6 +1,6 @@
 <nav class="four columns">
-    <a href="index.php?page=about">about</a>
-    <a href="index.php?page=portfolio">portfolio</a>
-    <a href="index.php?page=blog">blog</a>
-    <a href="index.php?page=contact">Contact</a>
+    <a href="index.php?page=about" <?php if(!isset($_GET['page']) || $_GET['page'] == 'about') {echo "class=\"active\""; }?>>about</a>
+    <a href="index.php?page=portfolio" <?php if($_GET['page'] == 'portfolio') {echo "class=\"active\""; }?>>portfolio</a>
+    <a href="index.php?page=blog" <?php if($_GET['page'] == 'blog') {echo "class=\"active\""; }?>>blog</a>
+    <a href="index.php?page=contact" <?php if($_GET['page'] == 'contact') {echo "class=\"active\""; }?>>Contact</a>
 </nav>
